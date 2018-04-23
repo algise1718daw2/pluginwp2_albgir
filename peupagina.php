e605f2562a454190bf9cd5ea809a51e0cda63de5
root <root@algise.fjeclot.net>
2018-04-23 21:18:54 +0200

    Commit 4

--- peupagina.php
+++ peupagina.php
@@ -14,10 +14,16 @@ function admin_menu_peu_pagina(){
 	$role="administrator";
 	$id="peu_pagina_per_defecte";
 	$nom_funcio="nouPeuPagDef";
-	add_menu_page($titolPagina,$titolEntrada,$role,$id);
+	add_menu_page($titolPagina,$titolEntrada,$role,$id,$nom_funcio);
 }
 function nouPeuPagDef(){
 	include_once('formPeuPag.php');
 }
+function afegPeuPagDef(){
+	echo "<div style='color: red;
+	font-size: 10px;
+	margin: 10px;'>".get_option('footer_text')."</div>";
+}
 add_action('admin_menu','admin_menu_peu_pagina');
+add_action('wp_footer','afegPeuPagDef');
 ?>
