a4003be5ba94eea4af83c8d74b7c95f8e3f08721
root <root@algise.fjeclot.net>
2018-04-23 20:05:16 +0200

    Commit 1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades 
+Plugin URI: https://github.com/algise1718daw2/pluginwp2_albgir.git
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: Albert Giron
+Author URI: https://github.com/algise1718daw2
+License: GNU GPLv3 
+*/
+?>
