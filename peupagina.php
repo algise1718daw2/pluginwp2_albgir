82f69045d1c4b7c99bb30d69cd28afe61730155c
root <root@algise.fjeclot.net>
2018-04-23 20:50:24 +0200

    Commit 2

--- peupagina.php
+++ peupagina.php
@@ -1,11 +1,19 @@
 <?php
 /*
-Plugin Name: Peu de pàgina per defecte per a noves entrades 
+Plugin Name: Peu de pàgina per defecte per a noves entrades
 Plugin URI: https://github.com/algise1718daw2/pluginwp2_albgir.git
 Description: Crea peus de pàgina per defecte per a noves entrades
 Version: 0.1
 Author: Albert Giron
 Author URI: https://github.com/algise1718daw2
-License: GNU GPLv3 
+License: GNU GPLv3
 */
+function admin_menu_peu_pagina(){
+	$titolPagina="Peu de pàgina per defecte";
+	$titolEntrada="Peu de pàgina per defecte";
+	$role="administrator";
+	$id="peu_pagina_per_defecte";
+	add_menu_page($titolPagina,$titolEntrada,$role,$id);
+}
+add_action('admin_menu','admin_menu_peu_pagina');
 ?>
